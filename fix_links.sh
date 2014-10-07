6551ca87123f489606d9b617914e6399d644dcba
Ungureanu Marius <marius.ungureanu@xamarin.com>
2014-10-07 10:22:32 +0300

    [Script] Update it to be clearer about usage.

--- fix_links.sh
+++ fix_links.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd mac
+# This script is used to make dylibs installed for /usr/local be portable.
+
+# Change this to be the path to the dylibs.
+LIBRARY_DIR=mac
+
+pushd $LIBRARY_DIR
 for i in *; do
 	install_name_tool -id "$i" "$i"
 	for j in *; do
@@ -8,4 +13,5 @@ for i in *; do
 		install_name_tool -change "/usr/local/lib/$j" "@loader_path/$j" "$i"
 	done
 done
+popd
 
